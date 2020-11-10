3e6fd618e48718de9b867678dccadbc4bc05efd7
deu93 <deucalion93@ukr.net>
2020-11-10 11:09:00 +0200

    scm

--- main.py
+++ main.py
@@ -11,13 +11,19 @@ wn.tracer(0)
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
-paddle_a.color("white")
+paddle_a.color("green")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
 # Paddle B
-
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("purple")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
 
 
 # Ball
